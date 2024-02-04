4a520c0ce789ea9522dcb7b0850166438b8701a2
broma0 <121064585+broma0@users.noreply.github.com>
2024-02-04 16:49:51 +0000

    posix.c pread.lua sh.lua system.lua: update helper, run_child +7
    lib/santoku/system.lua
      + execute
      - M.execute
    lib/santoku/system/posix.c
      + tk_system_callmod
    lib/santoku/system/pread.lua
      + helper
      ~ run_child, run_parent, run_parent_loop
    lib/santoku/system/sh.lua
      + helper
      - process_events, yield_results
    
    also: docs/todo.md, make.lua, test/spec/santoku/system.lua

--- lib/santoku/system/posix.c
+++ lib/santoku/system/posix.c
@@ -7,12 +7,25 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+
+void tk_system_callmod (lua_State *L, int nargs, int nret, const char *smod, const char *sfn)
+{
+  lua_getglobal(L, "require");
+  lua_pushstring(L, smod);
+  lua_call(L, 1, 1);
+  lua_pushstring(L, sfn);
+  lua_gettable(L, -2);
+  lua_remove(L, -2);
+  lua_insert(L, - nargs - 1);
+  lua_call(L, nargs, nret);
+}
+
 int tk_system_posix_err (lua_State *L, int err)
 {
-  lua_pushboolean(L, 0);
   lua_pushstring(L, strerror(errno));
   lua_pushinteger(L, err);
-  return 3;
+  tk_system_callmod(L, 2, 0, "santoku.error", "error");
+  return 0;
 }
 
 int tk_system_posix_close (lua_State *L)
@@ -21,8 +34,7 @@ int tk_system_posix_close (lua_State *L)
   int rc = close(n);
   if (rc == -1)
     return tk_system_posix_err(L, errno);
-  lua_pushboolean(L, 1);
-  return 1;
+  return 0;
 }
 
 
@@ -39,10 +51,9 @@ int tk_system_posix_read (lua_State *L)
     free(buf);
     return tk_system_posix_err(L, err);
   }
-  lua_pushboolean(L, 1);
   lua_pushlstring(L, buf, bytes);
   free(buf);
-  return 2;
+  return 1;
 }
 
 int tk_system_posix_setenv (lua_State *L)
@@ -51,8 +62,7 @@ int tk_system_posix_setenv (lua_State *L)
   const char *v = luaL_checkstring(L, 2);
   if (setenv(k, v, 1) == -1)
     return tk_system_posix_err(L, errno);
-  lua_pushboolean(L, 1);
-  return 1;
+  return 0;
 }
 
 int tk_system_posix_dup2 (lua_State *L)
@@ -62,8 +72,7 @@ int tk_system_posix_dup2 (lua_State *L)
   int rc = dup2(old, new);
   if (rc == -1)
     return tk_system_posix_err(L, errno);
-  lua_pushboolean(L, 1);
-  return 1;
+  return 0;
 }
 
 int tk_system_posix_pipe (lua_State *L)
@@ -72,10 +81,9 @@ int tk_system_posix_pipe (lua_State *L)
   int rc = pipe(fds);
   if (rc == -1)
     return tk_system_posix_err(L, errno);
-  lua_pushboolean(L, 1);
   lua_pushinteger(L, fds[0]);
   lua_pushinteger(L, fds[1]);
-  return 3;
+  return 2;
 }
 
 int tk_system_posix_execp (lua_State *L)
@@ -101,9 +109,8 @@ int tk_system_posix_fork (lua_State *L)
   pid_t pid = fork();
   if (pid == -1)
     return tk_system_posix_err(L, errno);
-  lua_pushboolean(L, 1);
   lua_pushinteger(L, pid);
-  return 2;
+  return 1;
 }
 
 int tk_system_posix_wait (lua_State *L)
@@ -113,27 +120,26 @@ int tk_system_posix_wait (lua_State *L)
   int pid1 = waitpid(pid0, &status, 0);
   if (pid1 == -1)
     return tk_system_posix_err(L, errno);
-  lua_pushboolean(L, 1);
   lua_pushinteger(L, pid1);
   if (WIFEXITED(status)) {
     lua_pushstring(L, "exited");
     lua_pushinteger(L, WEXITSTATUS(status));
-    return 4;
+    return 3;
   } else if (WIFSIGNALED(status)) {
     lua_pushstring(L, "signaled");
     lua_pushinteger(L, WTERMSIG(status));
-    return 4;
+    return 3;
   } else if (WIFSTOPPED(status)) {
     lua_pushstring(L, "stopped");
     lua_pushinteger(L, WSTOPSIG(status));
-    return 4;
+    return 3;
   } else if (WIFCONTINUED(status)) {
     lua_pushstring(L, "continued");
-    return 3;
+    return 2;
   } else {
     lua_pushstring(L, "unknown");
     lua_pushinteger(L, status);
-    return 3;
+    return 2;
   }
 }
 
@@ -154,5 +160,7 @@ int luaopen_santoku_system_posix (lua_State *L)
 {
   lua_newtable(L);
   luaL_register(L, NULL, tk_system_posix_fns);
+  lua_pushinteger(L, BUFSIZ);
+  lua_setfield(L, -2, "BUFSIZ");
   return 1;
 }
