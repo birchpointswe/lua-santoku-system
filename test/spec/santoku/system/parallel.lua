19ec0f9dfb8a10d94abce3f792b47a1ad2029f8e
broma0 <121064585+broma0@users.noreply.github.com>
2025-03-10 15:40:43 +0000

    parallel.lua posix.c: add peek_mutex, tk_mutex, tk_mutex_closure +2
    lib/santoku/system/posix.c
      + peek_mutex, tk_mutex, tk_mutex_closure, tk_mutex_destroy
    test/spec/santoku/system/parallel.lua
      + fn
    
    also: make.lua

--- test/spec/santoku/system/parallel.lua
+++ test/spec/santoku/system/parallel.lua
@@ -1,24 +1,26 @@
+local sys = require("santoku.system")
+local counter = sys.atom(1, 0.1)
 
+local vals = {}
+for i = 1, 100 do
+  vals[i] = i
+end
 
+local mutex = sys.mutex()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for data in sys.sh({
+  jobs = 10,
+  fn = function (job)
+    while true do
+      if a > #vals then
+        break
+      else
+        sys.sleep(1)
+        print(job, a, vals[a])
+        io.stdout:flush()
+      end
+    end
+  end
+}) do
+  print("  result", data)
+end
